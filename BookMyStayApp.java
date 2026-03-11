df40996d48d170d0c9a1a072e20b7c4e9d56beca
suvarnaj3 <suvarnajaiswal3@gmail.com>
2026-03-11 09:56:06 +0000

    implemented entry point for application

---
+++ BookMyStayApp.java
@@ -0,0 +1,22 @@
+/**
+ * UseCase1HotelBookingApp serves as the entry point for the Hotel Booking Management System.
+ * This class demonstrates the basic structure of a Java application, including the main method,
+ * static keyword, and console output.
+ * 
+ * @author DEVELOP
+ * @version 1.0
+ */
+public class HotelBookingApp {
+
+    /**
+     * The main method is the starting point of the application.
+     * It prints a welcome message and the application version to the console.
+     * 
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay App");
+        System.out.println("Hotel Booking System v1.0");
+        
+    }
+}
