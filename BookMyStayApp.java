4b8cdb447b42e2c52ae8b577be03af846e01db75
suvarnaj3 <suvarnajaiswal3@gmail.com>
2026-03-11 10:08:24 +0000

    implement UC3 RoomInventory

--- BookMyStayApp.java
+++ BookMyStayApp.java
@@ -1,42 +1,32 @@
+import java.util.Map;
+
 /**
- * MAIN CLASS - UseCase2RoomInitialization
- * Demonstrates room initialization and static availability.
- * @version 2.1
+ * MAIN CLASS - UseCase3InventorySetup
+ * Demonstrates centralized inventory management using HashMap.
+ * @version 3.1
  */
-public class HostelBookingApp {
-    class SingleRoom extends Room {
-        public SingleRoom() { super(1, 250, 1500.0); }
-    }
-
-    class DoubleRoom extends Room {
-        public DoubleRoom() { super(2, 400, 2500.0); }
-    }
-
-    class SuiteRoom extends Room {
-        public SuiteRoom() { super(3, 750, 5000.0); }
-    }
+public class HotelBookingApp {
 
     public static void main(String[] args) {
-        System.out.println("Hotel Room Initialization\n");
+        System.out.println("Hotel Room Inventory Status\n");
+
+        RoomInventory inventory = new RoomInventory();
+        Map<String, Integer> currentStatus = inventory.getRoomAvailability();
 
         Room single = new SingleRoom();
         Room doubleRm = new DoubleRoom();
         Room suite = new SuiteRoom();
 
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
-        int suiteAvailable = 2;
-
         System.out.println("Single Room:");
         single.displayRoomDetails();
-        System.out.println("Available: " + singleAvailable + "\n");
+        System.out.println("Available Rooms: " + currentStatus.get("Single Room") + "\n");
 
         System.out.println("Double Room:");
         doubleRm.displayRoomDetails();
-        System.out.println("Available: " + doubleAvailable + "\n");
+        System.out.println("Available Rooms: " + currentStatus.get("Double Room") + "\n");
 
         System.out.println("Suite Room:");
         suite.displayRoomDetails();
-        System.out.println("Available: " + suiteAvailable);
+        System.out.println("Available Rooms: " + currentStatus.get("Suite Room"));
     }
 }
